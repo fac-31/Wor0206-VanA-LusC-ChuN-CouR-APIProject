50b2c18f2fce4c915421365c31e7fa46d89aa78c
nchua3012 <n.chua3012@googlemail.com>
2025-02-10 10:11:02 +0000

    Initial commit NC API

---
+++ public/nick/script.js
@@ -0,0 +1,62 @@
+async function fetchPokemonAbility() {
+    const pokemonName = document.getElementById('pokemon').value.toLowerCase();  
+
+    if (pokemonName) {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            const data = await response.json();
+
+            if (data.abilities) {
+                
+                const firstAbility = data.abilities[0].ability.name;
+                console.log(data.abilities); 
+
+                const firstLetter = firstAbility.charAt(0).toUpperCase();
+                console.log(firstLetter)
+
+                document.getElementById('result').textContent = ` ${pokemonName} uses ${firstAbility}!`;
+
+                fetchDogBreed(firstLetter);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            document.getElementById('result').textContent = 'Error fetching data. Please check the Pokémon name.';
+        }
+    }
+}
+
+async function fetchDogBreed(firstLetter) {
+    try {
+        
+        const response = await fetch('https://dog.ceo/api/breeds/list/all');
+        console.log(response);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch dog breeds');
+        }
+
+        const data = await response.json();
+        const breeds = Object.keys(data.message); 
+        const filteredBreeds = breeds.filter(breed => breed.charAt(0).toLowerCase() === firstLetter.toLowerCase());
+
+
+        if (filteredBreeds.length > 0) {
+            const randomBreed = filteredBreeds[Math.floor(Math.random() * filteredBreeds.length)];
+            document.getElementById('dog-breed').textContent = `A wild ${randomBreed} appears!`;
+        }
+        else {
+            document.getElementById('dog-breed').textContent = `No dog breed found for the letter '${firstLetter}'`;
+        }
+    } catch (error) {
+        console.error('Error fetching dog breed:', error);
+        document.getElementById('dog-breed').textContent = 'Error fetching dog breed. Please try again later.';
+    }
+}
+
+
+
+        document.getElementById('pokemonForm').addEventListener('submit', function(event) {
+            event.preventDefault(); 
+            fetchPokemonAbility();  
+            
+        });
